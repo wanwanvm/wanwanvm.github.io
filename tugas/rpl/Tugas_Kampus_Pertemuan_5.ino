3655658f5065d3dd3ea1be96ff78e3579579d76c
Miswan <75777428+wanwanvm@users.noreply.github.com>
2022-04-09 13:14:04 +0700

    Update Tugas_Kampus_Pertemuan_5.ino

--- tugas/rpl/Tugas_Kampus_Pertemuan_5.ino
+++ tugas/rpl/Tugas_Kampus_Pertemuan_5.ino
@@ -3,52 +3,52 @@
 //Magister Teknik Informatika
 //NIM 211012000035
 
-byte tombol= 13;
-byte led1 = 3;
-byte led2 = 5;
+byte tombol= 13; //pendefinisian
+const byte led1 = 3; // karena nilai tetap / konstan
+const byte led2 = 5;
 byte led3 = 8;
 int nilai;
 int counter = 0;
+
 void setup(){
-  pinMode(led1, OUTPUT);
+  // dibaca hanya satu kali //seting untuk PIN 
+  pinMode(led1, OUTPUT); 
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(tombol, INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);//BAUD rate kecepatan 
 }
 
 void loop()
 {
   nilai= digitalRead(tombol);
-
+  delay(250); //bouncing
   if(nilai == 0){
-   // digitalWrite(led, HIGH);
+   counter++;
     Serial.print("Push ke = ");
     Serial.println(counter);
-    counter++;
-    delay(1000);
    
   }
   
-  if(counter == 6)
+  if(counter == 5)
   {
     digitalWrite(led1, HIGH);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
   }
-  else if(counter == 7)
+  else if(counter == 6)
   {
     digitalWrite(led1, LOW);
     digitalWrite(led2, HIGH);
     digitalWrite(led3, LOW);
   }
-  else if(counter == 8)
+  else if(counter == 7)
   {
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
   digitalWrite(led3, HIGH);
   }
- else if(counter == 9)
+ else if(counter == 8)
   {
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
