33dacf6f62ed3b732ad1110915fbace39ccefd44
Miswan <75777428+wanwanvm@users.noreply.github.com>
2022-04-16 00:21:53 +0700

    Add files via upload

---
+++ tugas/max6675/MAX6675.ino
@@ -0,0 +1,24 @@
+#include <max6675.h>
+
+int ktcSO = 8;
+int ktcCS = 9;
+int ktcCLK = 13;
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
+  
+void setup() {
+  Serial.begin(9600);
+  // give the MAX a little time to settle
+  delay(500);
+}
+
+void loop() {
+  // basic readout test
+  
+   Serial.print("Deg C = "); 
+   Serial.print(ktc.readCelsius());
+   Serial.print("t Deg F = ");
+   Serial.println(ktc.readFahrenheit());
+ 
+   delay(500);
+}
