be1b7b95ba67934cce07e7f81e0d566332df4f53
Miswan <75777428+wanwanvm@users.noreply.github.com>
2022-04-15 23:28:29 +0700

    Add files via upload

---
+++ tugas/relay/relay.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(8, HIGH);
+  delay(2000);
+  // put your main code here, to run repeatedly:
+
+}
